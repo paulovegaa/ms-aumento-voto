b04fea9de82965b784f95db3b6b45e4c232c80c1
paulovegaa <167259144+paulovegaa@users.noreply.github.com>
2024-04-24 20:05:33 -0500

    se cmbia / para agregar hola

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -27,7 +27,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps";
+			return "Hola Kubernetes DevSecOps !!!!";
 		}
 
 		@GetMapping("/compare/{value}")
